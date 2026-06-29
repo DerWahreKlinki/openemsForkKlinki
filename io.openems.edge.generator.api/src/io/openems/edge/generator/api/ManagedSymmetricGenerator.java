e235e593f865a7300a237a07b87827aed369ea09
DerWahreKlinki <thomas.klinkenberg@partec.org>
2026-06-29 07:03:47 +0200

    Add EC Power API and Controller

---
+++ io.openems.edge.generator.api/src/io/openems/edge/generator/api/ManagedSymmetricGenerator.java
@@ -0,0 +1,328 @@
+package io.openems.edge.generator.api;
+
+import io.openems.common.channel.AccessMode;
+import io.openems.common.channel.Level;
+import io.openems.common.channel.PersistencePriority;
+import io.openems.common.channel.Unit;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.common.channel.BooleanReadChannel;
+import io.openems.edge.common.channel.Doc;
+
+import io.openems.edge.common.channel.IntegerDoc;
+import io.openems.edge.common.channel.IntegerReadChannel;
+import io.openems.edge.common.channel.IntegerWriteChannel;
+import io.openems.edge.common.channel.StateChannel;
+import io.openems.edge.common.channel.value.Value;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.modbusslave.ModbusSlaveNatureTable;
+
+
+/**
+ * Represents a 3-Phase, symmetric PV-Inverter.
+ */
+public interface ManagedSymmetricGenerator extends OpenemsComponent, SymmetricGenerator  {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+
+
+		
+		AWAITING_HYSTERESIS(Doc.of(Level.WARNING) //
+				.text("Would change regulation, but hysteresis is active")),
+		
+		
+		READY_FOR_OPERATION(Doc.of(OpenemsType.BOOLEAN) //
+				.text("CHP hardware is ready for operation") //
+				.persistencePriority(PersistencePriority.HIGH)), //		
+		
+		/**
+		 * Holds the maximum possible apparent power. This value is defined by the
+		 * inverter limitations.
+		 *
+		 * <ul>
+		 * <li>Interface: SymmetricPvInverter
+		 * <li>Type: Integer
+		 * <li>Unit: VA
+		 * <li>Range: zero or positive value
+		 * </ul>
+		 */
+		GENRATOR_MAX_ACTIVE_POWER(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.WATT) //
+				.persistencePriority(PersistencePriority.MEDIUM)), //
+		/**
+		 * Holds the maximum possible apparent power. This value is defined by the
+		 * inverter limitations.
+		 *
+		 * <ul>
+		 * <li>Interface: SymmetricPvInverter
+		 * <li>Type: Integer
+		 * <li>Unit: VA
+		 * <li>Range: zero or positive value
+		 * </ul>
+		 */
+		GENRATOR_MAX_REACTIVE_POWER(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.VOLT_AMPERE_REACTIVE) //
+				.persistencePriority(PersistencePriority.MEDIUM)), //
+		/**
+		 * Holds the maximum possible apparent power. This value is defined by the
+		 * inverter limitations.
+		 *
+		 * <ul>
+		 * <li>Interface: SymmetricPvInverter
+		 * <li>Type: Integer
+		 * <li>Unit: VA
+		 * <li>Range: zero or positive value
+		 * </ul>
+		 */
+		GENRATOR_MAX_APPARENT_POWER(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.VOLT_AMPERE) //
+				.persistencePriority(PersistencePriority.MEDIUM)), //
+		/**
+		 * Read/Set Active Power Limit.
+		 *
+		 * <ul>
+		 * <li>Interface: PV-Inverter Symmetric
+		 * <li>Type: Integer
+		 * <li>Unit: W
+		 * </ul>
+		 */
+		GENRATOR_ACTIVE_POWER_LIMIT(new IntegerDoc() //
+				.unit(Unit.WATT) //
+				.accessMode(AccessMode.READ_WRITE) //
+				.persistencePriority(PersistencePriority.MEDIUM) //
+				.onInit(channel -> { //
+					// on each Write to the channel -> set the value
+					((IntegerWriteChannel) channel).onSetNextWrite(value -> {
+						channel.setNextValue(value);
+					});
+				}));
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+
+	
+	/**
+	 * Gets the Channel for {@link ChannelId#AWAITING_HYSTERESIS}.
+	 *
+	 * @return the Channel
+	 */
+	public default StateChannel getAwaitingHysteresisChannel() {
+		return this.channel(ChannelId.AWAITING_HYSTERESIS);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#AWAITING_HYSTERESIS} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setAwaitingHysteresis(boolean value) {
+		this.getAwaitingHysteresisChannel().setNextValue(value);
+	}		
+	
+	public default Value<Boolean> getAwaitingHysteresis() {
+		return this.getAwaitingHysteresisChannel().value();
+	}	
+	
+
+	/**
+	 * Gets the Channel for {@link ChannelId#MAX_ACTIVE_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getGeneratorMaxActivePowerChannel() {
+		return this.channel(ChannelId.GENRATOR_MAX_ACTIVE_POWER);
+	}
+
+	
+
+	/**
+	 * Gets the Maximum Active Power in [WATT], range "&gt;= 0". See
+	 * {@link ChannelId#MAX_ACTIVE_POWER}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getGeneratorMaxActivePower() {
+		return this.getGeneratorMaxActivePowerChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#MAX_ACTIVE_POWER}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorMaxActivePower(Integer value) {
+		this.getGeneratorMaxActivePowerChannel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#MAX_ACTIVE_POWER}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorMaxActivePower(int value) {
+		this.getGeneratorMaxActivePowerChannel().setNextValue(value);
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#MAX_REACTIVE_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getGeneratorMaxReactivePowerChannel() {
+		return this.channel(ChannelId.GENRATOR_MAX_REACTIVE_POWER);
+	}
+
+	/**
+	 * Gets the Maximum Reactive Power in [VAR], range "&gt;= 0". See
+	 * {@link ChannelId#MAX_REACTIVE_POWER}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getGeneratorMaxReactivePower() {
+		return this.getGeneratorMaxReactivePowerChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#MAX_REACTIVE_POWER} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorMaxReactivePower(Integer value) {
+		this.getGeneratorMaxReactivePowerChannel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#MAX_REACTIVE_POWER} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setMaxReactivePower(int value) {
+		this.getGeneratorMaxReactivePowerChannel().setNextValue(value);
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#MAX_APPARENT_POWER}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getGeneratorMaxApparentPowerChannel() {
+		return this.channel(ChannelId.GENRATOR_MAX_APPARENT_POWER);
+	}
+
+	/**
+	 * Gets the Maximum Apparent Power in [VA], range "&gt;= 0". See
+	 * {@link ChannelId#MAX_APPARENT_POWER}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getGeneratorMaxApparentPower() {
+		return this.getGeneratorMaxApparentPowerChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#MAX_APPARENT_POWER} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorMaxApparentPower(Integer value) {
+		this.getGeneratorMaxApparentPowerChannel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#MAX_APPARENT_POWER} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorMaxApparentPower(int value) {
+		this.getGeneratorMaxApparentPowerChannel().setNextValue(value);
+	}
+	
+	//
+	/**
+	 * Gets the Channel for {@link ChannelId#ACTIVE_POWER_LIMIT}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerWriteChannel getGeneratorActivePowerLimitChannel() {
+		return this.channel(ChannelId.GENRATOR_ACTIVE_POWER_LIMIT);
+	}
+
+	/**
+	 * Gets the Active Power Limit in [W]. See {@link ChannelId#ACTIVE_POWER_LIMIT}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getGeneratorActivePowerLimit() {
+		return this.getGeneratorActivePowerLimitChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#ACTIVE_POWER_LIMIT} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorActivePowerLimit(Integer value) {
+		this.getGeneratorActivePowerLimitChannel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#ACTIVE_POWER_LIMIT} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorActivePowerLimit(int value) {
+		this.getGeneratorActivePowerLimitChannel().setNextValue(value);
+	}
+	
+
+	//
+	public default BooleanReadChannel getReadyForOperationChannel() {
+		return this.channel(ChannelId.READY_FOR_OPERATION);
+	}
+
+	public default Value<Boolean> getReadyForOperation() {
+		return this.getReadyForOperationChannel().value();
+	}
+
+	public default void _setReadyForOperation(Boolean value) {
+		this.getReadyForOperationChannel().setNextValue(value);
+	}	
+
+	
+
+	/**
+	 * Used for Modbus/TCP Api Controller. Provides a Modbus table for the Channels
+	 * of this Component.
+	 *
+	 * @param accessMode filters the Modbus-Records that should be shown
+	 * @return the {@link ModbusSlaveNatureTable}
+	 */
+	public static ModbusSlaveNatureTable getModbusSlaveNatureTable(AccessMode accessMode) {
+		return ModbusSlaveNatureTable.of(ManagedSymmetricGenerator.class, accessMode, 100) //
+				.build();
+	}
+
+	public void applyPower(int calculateChpPowerTarget);
+
+	public void applyPower(Integer activePowerTarget);
+
+	public void applyPreparation(Boolean activate);
+	
+}
---
+++ io.openems.edge.generator.api/src/io/openems/edge/generator/api/SymmetricGenerator.java
@@ -0,0 +1,105 @@
+package io.openems.edge.generator.api;
+
+import io.openems.common.channel.PersistencePriority;
+import io.openems.common.channel.Unit;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.channel.IntegerReadChannel;
+import io.openems.edge.common.channel.value.Value;
+import io.openems.edge.common.component.OpenemsComponent;
+
+/**
+ * Represents a 3-Phase, symmetric CHP
+ */
+public interface SymmetricGenerator extends   OpenemsComponent {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		 
+		GENERATOR_ACTIVE_POWER(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.WATT) //
+				.persistencePriority(PersistencePriority.MEDIUM)), //
+		
+		GENERATOR_HEAT_POWER(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.WATT) //
+				.persistencePriority(PersistencePriority.MEDIUM)), //
+		
+		BUFFER_TANK_TEMPERATURE(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.DEZIDEGREE_CELSIUS) //
+				.persistencePriority(PersistencePriority.MEDIUM)), //		
+	
+		;
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+	
+	//
+	public default IntegerReadChannel getAverageBufferTankTemperatureChannel() {
+		return this.channel(ChannelId.BUFFER_TANK_TEMPERATURE);
+	}
+
+	/**
+	 * Gets the Active Power  in [W]. See {@link ChannelId#ACTIVE_POWER_}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getAverageBufferTankTemperature() {
+		return this.getAverageBufferTankTemperatureChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#ACTIVE_POWER_} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setAverageBufferTankTemperature(Integer value) {
+		this.getAverageBufferTankTemperatureChannel().setNextValue(value);
+	}	
+
+
+	//
+	public default IntegerReadChannel getGeneratorActivePowerChannel() {
+		return this.channel(ChannelId.GENERATOR_ACTIVE_POWER);
+	}
+
+	/**
+	 * Gets the Active Power  in [W]. See {@link ChannelId#ACTIVE_POWER_}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getGeneratorActivePower() {
+		return this.getGeneratorActivePowerChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#ACTIVE_POWER_} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorActivePower(Integer value) {
+		this.getGeneratorActivePowerChannel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#ACTIVE_POWER_} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setGeneratorActivePower(int value) {
+		this.getGeneratorActivePowerChannel().setNextValue(value);
+	}		
+	
+
+
+}

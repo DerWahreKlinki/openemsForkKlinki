581a627769b812e1c7c13b602f216551e265f09b
DerWahreKlinki <102486171+DerWahreKlinki@users.noreply.github.com>
2026-07-09 09:42:38 +0200

    UI fix: Added Generator-API (i.e. for displaying CHPs)

--- ui/src/app/shared/components/edge/edgeconfig.ts
+++ ui/src/app/shared/components/edge/edgeconfig.ts
@@ -591,7 +591,8 @@ export class EdgeConfig {
         }
         const natureIds = this.getNatureIdsByFactoryId(component.factoryId);
         if (natureIds.includes("io.openems.edge.pvinverter.api.ManagedSymmetricPvInverter")
-            || natureIds.includes("io.openems.edge.ess.dccharger.api.EssDcCharger")) {
+            || natureIds.includes("io.openems.edge.ess.dccharger.api.EssDcCharger")
+            || natureIds.includes("io.openems.edge.generator.api.SymmetricGenerator")) {
             return true;
         }
         // TODO properties in OSGi Component annotations are not transmitted correctly with Apache Felix SCR
